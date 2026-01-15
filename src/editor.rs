ab3361674be861682732833d210282b837d8ae0f
Justin Tieri <37750742+jtieri@users.noreply.github.com>
2026-01-15 00:02:29 -0600

    feat: add support for moving the caret around + light refactor to welcome message to align with the guide

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,5 @@
-use crossterm::{event::{Event::{self, Key}, KeyCode::Char, KeyEvent, KeyModifiers, read}};
-use std::{fmt::format, io::Error};
+use crossterm::{event::{Event::{self, Key}, KeyCode, KeyEvent, KeyModifiers, read}};
+use std::{io::Error};
 use terminal::{Terminal, Size, Position};
 
 mod terminal;
@@ -9,11 +9,12 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
+    position: Position
 }
 
 impl Editor {
     pub const fn default() -> Self {
-        Self { should_quit: false }
+        Self { should_quit: false, position: Position { x: 0, y: 0 } }
     }
 
     pub fn run(&mut self) {
@@ -37,10 +38,52 @@ impl Editor {
     }
 
     fn evaluate_event(&mut self, event: &Event) {
-        if let Key(KeyEvent {code: Char('q'), modifiers: KeyModifiers::CONTROL, ..}) = event
-        {
-            self.should_quit = true;
-        }
+        let Size{ height, width } = Terminal::size().unwrap(); 
+    
+        if let Key(KeyEvent { code, modifiers, kind: _kind, state: _state }) = event {
+            match code {
+                KeyCode::Left => {
+                    // if we are at the left edge of the screen we don't update the position
+                    if self.position.x > 0 {
+                        self.position = Position { x: self.position.x-1, y: self.position.y };
+                    } 
+                }
+                KeyCode::Right => {
+                    // if we are at the right edge of the screen we don't update the position
+                    if self.position.x < width-1 {
+                        self.position = Position { x: self.position.x+1, y: self.position.y};
+                    }
+                }
+                KeyCode::Up => {
+                    // if we are at the top of the screen we don't update the position
+                    if self.position.y > 0 {
+                        self.position = Position { x: self.position.x, y: self.position.y-1 };
+                    } 
+                }
+                KeyCode::Down => {
+                    // if we are at the bottom of the screen we don't update the position
+                    if self.position.y < height-1 {
+                        self.position = Position { x: self.position.x, y: self.position.y+1 };
+                    } 
+                }
+                KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                KeyCode::PageUp => {
+                    self.position = Position { x: self.position.x, y: 0 };
+                }
+                KeyCode::PageDown => {
+                    self.position = Position { x: self.position.x, y: height-1 };
+                }
+                KeyCode::Home => {
+                    self.position = Position { x: 0, y: self.position.y };
+                }
+                KeyCode::End => {
+                    self.position = Position { x: width-1, y: self.position.y };
+                }
+                _ => ()
+            }
+        } 
     }
 
     fn refresh_screen(&self) -> Result<(), Error> {
@@ -51,51 +94,49 @@ impl Editor {
             Terminal::print("Goodbye.\r\n")?;
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(Position{x: 0, y: 0})?;
+            Terminal::move_cursor_to(self.position)?;
         }   
         
         Terminal::show_cursor()?;
         Terminal::execute()
     }
     
-    // fn draw_welcome_message() -> Result<(), Error> { 
-    //     let mut message = format!("{NAME} editor -- version {VERSION}");
-    //     let width = Terminal::size()?.width as usize;
-    //     let len = message.len();
-    //     let padding = (width - len) / 2;
-    //     let spaces = " ".repeat(padding - 1);
-    //     message = format!("~{spaces}{message}");
-    //     message.truncate(width);
-    //     Terminal::print(&message)
-    // } 
-    
-    fn draw_welcome_message() -> Result<(), Error> { 
-        let message = format!("{NAME} editor -- version {VERSION}");
-        Terminal::print(&message)
-    } 
+    fn draw_welcome_message() -> Result<(), Error> {
+            let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+            let width = Terminal::size()?.width;
+            let len = welcome_message.len();
+            
+            // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
+            // it's allowed to be a bit to the left or right.
+            #[allow(clippy::integer_division)]
+            let padding = (width.saturating_sub(len)) / 2;
+            let spaces = " ".repeat(padding.saturating_sub(1));
+            welcome_message = format!("~{spaces}{welcome_message}");
+            welcome_message.truncate(width);
+            Terminal::print(welcome_message)?;
+            Ok(())
+        }
     
     fn draw_empty_row() -> Result<(), Error> {
         Terminal::print("~")
     }
     
     fn draw_rows() -> Result<(), Error> {
-        let Size{height, width} = Terminal::size()?;
-        let middle_height = height / 3;
-        let center_row = width / 2;
-        
-        for current_row in 0..height {
-            Terminal::clear_line()?;
-            Self::draw_empty_row()?;
-            
-            if current_row ==  middle_height {
-                Terminal::move_cursor_to(Position { x: center_row, y: middle_height })?;
-                Self::draw_welcome_message()?;
+            let Size { height, .. } = Terminal::size()?;
+            for current_row in 0..height {
+                Terminal::clear_line()?;
+                // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
+                // it's allowed to be a bit up or down
+                #[allow(clippy::integer_division)]
+                if current_row == height / 3 {
+                    Self::draw_welcome_message()?;
+                } else {
+                    Self::draw_empty_row()?;
+                }
+                if current_row.saturating_add(1) < height {
+                    Terminal::print("\r\n")?;
+                }
             }
-            
-            if current_row + 1 < height {
-                Terminal::print("\r\n")?;
-            }
+            Ok(())
         }
-        Ok(())
-    }
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -6,18 +6,24 @@ use crossterm::terminal::size as raw_size;
 use std::io::{Error, Write, stdout};
 use core::fmt::Display;
 
+/// Represents the Terminal.
+/// Edge Case for platforms where `usize` < `u16`:
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
+/// And should you attempt to set the cursor out of these bounds, it will also be truncated.
 pub struct Terminal;
 
 #[derive(Copy, Clone)]
 pub struct Size {
-    pub width: u16,
-    pub height: u16
+    pub width: usize,
+    pub height: usize
 }
 
 #[derive(Copy, Clone)]
 pub struct Position {
-   pub x: u16,
-   pub y: u16
+   pub x: usize,
+   pub y: usize
 }
 
 impl Terminal {
@@ -41,8 +47,13 @@ impl Terminal {
         Self::queue_command(Clear(ClearType::CurrentLine))
     }
 
+    /// Moves the cursor to the given Position.
+    /// # Arguments
+    /// * `Position` - the  `Position`to move the cursor to. Will be truncated to `u16::MAX` if bigger.
     pub fn move_cursor_to(pos: Position) -> Result<(), Error> {
-        Self::queue_command(MoveTo(pos.x, pos.y))
+        // clippy::as_conversions: See doc above
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(pos.x as u16, pos.y as u16))
     }
     
     pub fn hide_cursor() -> Result<(), Error> {
@@ -56,12 +67,24 @@ impl Terminal {
     pub fn print<T: Display>(string: T) -> Result<(), Error> {
         Self::queue_command(Print(string))
     }
-
+    
+    /// Returns the current size of this Terminal.
+    /// Edge Case for systems with `usize` < `u16`:
+    /// * A `Size` representing the terminal size. Any coordinate `z` truncated to `usize` if `usize` < `z` < `u16`
     pub fn size() -> Result<Size, Error> {
-        let (width, height) = raw_size()?;
+        let (width_u16, height_u16) = raw_size()?;  
+        
+        // clippy::as_conversions: See doc above
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+        
+        // clippy::as_conversions: See doc above
+        #[allow(clippy::as_conversions)]
+        let width = width_u16 as usize;
+        
         Ok(Size { width, height})
     }
-    
+
     pub fn execute() -> Result<(), Error> {
         stdout().flush()
     }
